462ad0fd3cdda7989d77be1e3bcf65c176ed1bb2
CrashedOut93 <crashstocks@gmail.com>
2021-10-11 17:04:46 -0500

    commit tree 881923f75a87599c9062f48dcb5330cfe1e2263e

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1= 31;
+let num2 = 2;
+let multiply = num1 * num2;
+multiply; 
 
-// Write your code here
+let random = Math.floor(Math.random() * 10);
+
+let num3 = 4;
+let num4 = 5;
+let mod = num3 % num4;
+mod; 
+
+let max = Math.max(1, 5, 10, 20);
